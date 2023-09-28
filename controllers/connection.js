685f95720e7660654369dc3a15dd49536856c22c
Sean Worrell <spworrell@gmail.com>
2023-09-28 12:03:19 -0400

    folders

---
+++ controllers/connection.js
@@ -0,0 +1,8 @@
+const Sequelize = require('sequelize');
+require('dotenv').config()
+
+
+// Create a connection object
+const sequelize = new Sequelize(process.env.MYSQLURI);
+
+module.exports = sequelize;
