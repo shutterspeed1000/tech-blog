e3b9d185c7ec0cb811f8337eca6d29430c84b630
Sean Worrell <spworrell@gmail.com>
2023-10-08 17:10:43 -0400

    HEroku1

--- config/connection.js
+++ config/connection.js
@@ -3,6 +3,6 @@ require('dotenv').config()
 
 
 // Create a connection object
-const sequelize = new Sequelize(process.env.MYSQLURI);
+const sequelize = new Sequelize(process.env.JAWDB_URL);
 
 module.exports = sequelize;
--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
-  secret: 'Super secret secret',
+  secret: process.env.SECRET,
   cookie: {
     maxAge: 300000,
     httpOnly: true,
